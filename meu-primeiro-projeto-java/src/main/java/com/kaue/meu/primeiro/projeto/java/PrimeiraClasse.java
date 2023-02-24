52933ef5de6a0608f39ed328c595a4d28e4748e3
kauevsantus <kaue.vsantos@sptech.school>
2023-02-24 18:17:05 -0300

    meu primeiro projeto em java

---
+++ meu-primeiro-projeto-java/src/main/java/com/kaue/meu/primeiro/projeto/java/PrimeiraClasse.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kaue.meu.primeiro.projeto.java;
+
+/**
+ *
+ * @author rails
+ */
+
+// Classes guardam nosso código em arquivos (.java)
+// Métodos em Java são equivalentes ao que chamavamos de função em JS 
+// Argumentos em Java são equivalentes ao que chamavamos de parmetros no JS
+
+public class PrimeiraClasse {
+    // Podemos criar o main com os atalhos:
+    // main tab
+    // psvm tab
+   
+    public static void main(String[] args) {
+        // console.log no Java
+        // atalho: sout tab
+        System.out.println("Hello World!");
+        
+        String nome = "Kauê";
+        System.out.println("Meu nome é: " + nome);
+        
+        int idade = 17;
+        double peso = 63.4;
+        
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Peso: " + peso + " Kg");
+    }
+ 
+}
+
+
